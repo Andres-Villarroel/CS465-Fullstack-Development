c3046a4f992a0fff70e901995d36bbd8963c8378
AndresVillarroel <andres121231@gmail.com>
2023-04-03 20:47:49 -0400

    Used Angular framwork to implement trip CRUD features

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -1,10 +1,10 @@
+const { model } = require("mongoose"); //new
 const mongoose = require('mongoose');
-const model = mongoose.model('trips');
+const Model = mongoose.model('trips');
 
 //GET: /trips - lists all the trips
 const tripsList = async(req, res) => {
-    model.find({})
-    .exec((err,trips) => {  //passing an error object and a list of trips
+    Model.find({}).exec((err,trips) => {  //passing an error object and a list of trips
         if(!trips){ //if 'not trips', in other words, if trips is empty... 
             return res
             .status(404)    //...return a 'res'ponse with an http status code of 404...
@@ -19,10 +19,9 @@ const tripsList = async(req, res) => {
     });
 };
 
-//GET: /trips/:tripCode - returns a single trip
 const tripsFindCode = async (req, res) => {
-    model
-        .find({ 'code': req.params.tripCode })
+    Model
+        .find({ code: req.params.tripCode })
         .exec((err, trip) => {
             if (!trip) {
                 return res
@@ -39,7 +38,78 @@ const tripsFindCode = async (req, res) => {
         });
 };
 
+
+const tripsAddTrip = async (req, res) => {
+    Model.create(
+    {
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description
+    },
+    (err, trip) => {
+        if (err) {
+            return res
+            .status(400)
+            .json(err);
+        } else {
+            return res
+            .status(201)
+            .json(trip);
+        }
+    }
+    );
+};
+
+const tripsUpdateTrip = async (req, res) => {
+    console.log(req.body);
+    Model
+        .findOneAndUpdate({ code: req.params.tripCode }, {
+            code: req.body.code,
+            name: req.body.name,
+            length: req.body.length,
+            start: req.body.start,
+            resort: req.body.resort,
+            perPerson: req.body.perPerson,
+            image: req.body.image,
+            description: req.body.description
+        }, 
+        { new: true }
+        )
+        .then((trip) => {
+            if (!trip) {
+                return res
+                    .status(404)
+                    .send({
+                        message: "Trip not found with code "
+                            + req.params.tripCode
+                    });
+            }
+            res.send(trip);
+        })
+        .catch(err => {
+            if (err.kind === 'ObjectId') {
+                return res
+                    .status(404)
+                    .send({
+                        message: "Trip not found with code "
+                            + req.params.tripCode
+                    });
+            }
+            return res
+                .status(500) // server error
+                .json(err);
+        });
+};
+
 module.exports = {
     tripsList,
-    tripsFindCode
+    tripsFindCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 };
+
--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,57 @@
-const fs = require('fs');
-const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+const request = require('request');
+const apiOptions = {
+    server: 'http://localhost:3000',
+};
 
+/* render travel list view*/
+const renderTravelList = (rec, res, responseBody) => {
+    let message = null;
+    let pageTitle = process.env.npm_package_description + ' - Travel';
+
+    //input validation
+    if (!(responseBody instanceof Array)) {
+        message = 'API lookup error';
+        responseBody = [];
+    } else {
+        if (!responseBody.length) {     //if responseBody length has no length or is empty...
+            message = 'No trips exist in database!' //...set the message accordingly
+        }
+    }
+    res.render('travel', {
+        title: pageTitle,
+        trips: responseBody,
+        message,
+    });
+};
+
+/* GET travel list view*/
+const travelList = (req, res) => {
+    const path = '/api/trips';  //period before first '/' caused the big error for during module 5 that prompted an email to instructor
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,
+        method: 'GET',
+        json: {},
+    };
+
+    console.info('>> travelController.travelList calling ' + requestOptions.url);
+
+    request(
+        
+        requestOptions,
+        (err, { statusCode }, body) => {
+            if (err) {
+                console.error(err);
+            }
+            renderTravelList(req, res, body);
+        }
+    )
+}
+
+module.exports = {
+    travelList
+};
+
+/*  OLD CODE    
 //require('./package.json')
 const travel = (req, res) => {
     pageTitle = process.env.npm_package_description + ' - Travel'
@@ -11,3 +62,4 @@ const travel = (req, res) => {
 module.exports = {
     travel
 }
+*/
